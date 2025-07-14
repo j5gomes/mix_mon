3224abe677f15232ea43a686b3932f1d8e85dc70
João Gomes <joaogomes@significa.co>
2025-07-14 16:33:21 +0100

    updating the game state

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,5 +1,6 @@
 defmodule MixMon.Game.Actions.Attack do
   alias MixMon.Game
+  alias MixMon.Game.Status
 
   @move_avg_power 18..25
   @move_rnd_power 10..35
@@ -11,7 +12,7 @@ defmodule MixMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:hp)
     |> calculate_total_hp(damage)
-    |> update_opponent_hp(opponent)
+    |> update_opponent_hp(opponent, damage)
   end
 
   defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
@@ -20,16 +21,18 @@ defmodule MixMon.Game.Actions.Attack do
   defp calculate_total_hp(hp, damage) when hp - damage < 0, do: 0
   defp calculate_total_hp(hp, damage), do: hp - damage
 
-  defp update_opponent_hp(hp, opponent) do
+  defp update_opponent_hp(hp, opponent, damage) do
     opponent
     |> Game.fetch_player()
     |> Map.put(:hp, hp)
-    |> update_game(opponent)
+    |> update_game(opponent, damage)
   end
 
-  defp update_game(player, opponent) do
+  defp update_game(player, opponent, damage) do
     Game.info()
     |> Map.put(opponent, player)
     |> Game.update()
+
+    Status.print_move_message(opponent, :attack, damage)
   end
 end
