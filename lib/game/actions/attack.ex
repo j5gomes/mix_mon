b10a80446dacdbe9a573cd16ce8f53ff9fa8921c
João Gomes <joaogomes@significa.co>
2025-07-14 14:47:04 +0100

    generating the hp value

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,11 +1,28 @@
 defmodule MixMon.Game.Actions.Attack do
+  alias MixMon.Game
+
   @move_avg_power 18..25
   @move_rnd_power 10..35
 
-  def attack_oponnent(oponnent, move) do
+  def attack_opponent(opponent, move) do
     damage = calculate_power(move)
+
+    opponent
+    |> Game.fetch_player()
+    |> Map.get(:hp)
+    |> calculate_total_hp(damage)
+    |> update_opponent_hp(opponent)
   end
 
-  def calculate_power(:move_avg), do: Enum.random(@move_avg_power)
-  def calculate_power(:move_rnd), do: Enum.random(@move_rnd_power)
+  defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
+  defp calculate_power(:move_rnd), do: Enum.random(@move_rnd_power)
+
+  defp calculate_total_hp(hp, damage) when hp - damage < 0, do: 0
+  defp calculate_total_hp(hp, damage), do: hp - damage
+
+  defp update_opponent_hp(hp, opponent) do
+    opponent
+    |> Game.fetch_player()
+    |> Map.put(:hp, hp)
+  end
 end
